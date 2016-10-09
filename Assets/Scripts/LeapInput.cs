1ac7f922edfc7af27f0025bc476d5070134a673d
Somn <somnacin7@gmail.com>
2016-10-08 22:16:48 -0400

    add hand rotation tracking

---
+++ Assets/Scripts/LeapInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Unity;
+
+public class LeapInput : MonoBehaviour
+{
+    public Transform stage;
+    LeapServiceProvider provider;
+
+    // Use this for initialization
+    void Start()
+    {
+        provider = FindObjectOfType<LeapServiceProvider>() as LeapServiceProvider;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Frame frame = provider.CurrentFrame;
+
+        foreach (Hand hand in frame.Hands)
+        {
+            if (hand.IsRight)
+            {
+                Debug.Log("Dir:\t" + hand.Direction.ToVector4());
+                
+                var dir = hand.Direction.ToVector3();
+                stage.rotation = hand.Rotation.ToQuaternion();
+            }
+        }
+    }
+}
