6c933951db54699c9d05fdda3ade78b8b8e60d85
Somn <somnacin7@gmail.com>
2016-10-09 14:24:33 -0400

    finished?

---
+++ Assets/Scripts/AudioSingleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSingleton : MonoBehaviour {
+
+    private static AudioSingleton instance = null;
+    public static AudioSingleton Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
+
